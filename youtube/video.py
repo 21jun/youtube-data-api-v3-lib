431b1762d1d76def00f28d9f729f1f40a5477065
21jun <21jun7654@gmail.com>
2020-01-31 03:39:33 +0900

    add tags and categoryId

--- youtube/video.py
+++ youtube/video.py
@@ -24,10 +24,11 @@ class YouTubeVideo:
         self.developer_auth = DEVELOPER_AUTH_LIST[developer_key_index]
         print(self.developer_key, self.developer_auth)
 
-        self.youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=self.developer_key)
+        self.youtube = build(YOUTUBE_API_SERVICE_NAME,
+                             YOUTUBE_API_VERSION, developerKey=self.developer_key)
         self.db = DataBase()
 
-    def fetch_all_games(self, condition=" where date='2020-01-14 00:00:00';"):
+    def fetch_all_games(self, condition=""):
         SQL = "SELECT appid, gameName from yt_videoidsearch group by appid"
         SQL = SQL + condition
         self.db.cur.execute(SQL)
@@ -49,12 +50,13 @@ class YouTubeVideo:
             self.db.cur.execute(SQL.format(name=name))
 
         video_ids = self.db.cur.fetchall()
-        video_ids = [{'appid': id[0], 'name': id[1], 'videoId': id[2]} for id in video_ids]
+        video_ids = [{'appid': id[0], 'name': id[1], 'videoId': id[2]}
+                     for id in video_ids]
         return video_ids
 
     def get_video_info_list(self, skip=0, file_path='TARGETLIST.txt', qtype="file", db_insert=True):
-        
-        if qtype=="file":
+
+        if qtype == "file":
             games = load_target_list(file_path)
         else:
             games = self.fetch_all_games()
@@ -70,10 +72,11 @@ class YouTubeVideo:
             video_ids = self.fetch_video_ids(game['appid'])
             print(len(video_ids))
             for j, video in enumerate(video_ids):
-                print(j, '/', len(video_ids), video['name'], '|', video['appid'], '|', video['videoId'])
-                self.get_video_info(game['appid'], game['name'], video['videoId'], db_insert=db_insert)
+                print(j, '/', len(video_ids),
+                      video['name'], '|', video['appid'], '|', video['videoId'])
+                self.get_video_info(
+                    game['appid'], game['name'], video['videoId'], db_insert=db_insert)
                 print("|", end="", flush=True)
-
 
     def get_video_info(self, appid, name, video_id, db_insert):
         video_info = self.youtube.videos().list(
@@ -86,10 +89,16 @@ class YouTubeVideo:
         except:
             print("video not found")
             return
+
         pub_date = parse(snippet['publishedAt']).strftime('%Y-%m-%d %H-%M-%S')
         channel_id = snippet['channelId']
         title = pymysql.escape_string(snippet.get('title', ""))
         description = pymysql.escape_string(snippet.get('description', ""))
+
+        tags = snippet.get('tags', [])
+        tags = ','.join(tags)[:1000]
+        tags = pymysql.escape_string(tags)
+        category_id = snippet.get('categoryId', "")
 
         statistics = video_info['items'][0]['statistics']
 
@@ -115,7 +124,9 @@ class YouTubeVideo:
         `favoriteCount`,
         `commentCount`,
         `pub_date`,
-        `date`)
+        `date`,
+        `categoryId`,
+        `tags`)
         VALUES
         (NULL,
         {appid},
@@ -130,14 +141,17 @@ class YouTubeVideo:
         {favorite_count},
         {comment_count},
         '{pub_date}',
-        '{date}');
+        '{date}',
+        {categoryId},
+        '{tags}');
         """
         if db_insert:
             try:
                 self.db.cur.execute(SQL.format(appid=appid, name=name, video_id=video_id, title=title, channel_id=channel_id,
-                                            description=description, view_count=view_count, like_count=like_count,
-                                            dislike_count=dislike_count,
-                                            favorite_count=favorite_count,
-                                            comment_count=comment_count, pub_date=pub_date, date=now))
+                                               description=description, view_count=view_count, like_count=like_count,
+                                               dislike_count=dislike_count,
+                                               favorite_count=favorite_count,
+                                               comment_count=comment_count, pub_date=pub_date, date=now,
+                                               categoryId=category_id, tags=tags))
             except Exception as e:
                 print(e)
